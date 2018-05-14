517ed7ee3dd91cb6bb8a338468cb57b064b7cc0f
impossibleno1 <37857357+impossibleno1@users.noreply.github.com>
2018-05-15 01:59:34 +0700

    login

---
+++ AppC/FormLogin.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace AppC
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(183, 53);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(183, 23);
+            this.txtusername.TabIndex = 1;
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(300, 192);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(66, 23);
+            this.btncancel.TabIndex = 2;
+            this.btncancel.Text = "Cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(183, 192);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(66, 23);
+            this.btnlogin.TabIndex = 4;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(183, 114);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(183, 23);
+            this.txtpassword.TabIndex = 5;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 366);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox txtpassword;
+    }
+}
---
+++ AppC/FormLogin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace AppC
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+
+             //Khai bao bien toan cuc ket noi vs sql
+       
+
+
+        }
+         Private 
+       
+
+    }
+}
